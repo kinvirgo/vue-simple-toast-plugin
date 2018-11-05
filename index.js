a873155cd47545f2f4c1d3b4dd30391b41bf63ae
jinyu.jiang <jinyu.jiang@flightroutes24.com>
2018-11-05 17:57:24 +0800

    移除es7写法

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
 require('./lib/vue-simple-toast-plugin.css');
-// es7
-import SimpleToast from './lib/vue-simple-toast-plugin'
 // es6
-const SimpleToast = require('./lib/vue-simple-toast-plugin')
-// export
-export default SimpleToast
+var SimpleToast = require('./lib/vue-simple-toast-plugin')
+module.exports = SimpleToast
